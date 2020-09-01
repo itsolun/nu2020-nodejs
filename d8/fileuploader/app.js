a37af60683bc09a0adb7b760711f4c355503822c
itsolun <60519459+itsolun@users.noreply.github.com>
2020-09-01 23:26:20 +0300

    show static files in path
    show static files in uploads folder path 
    example:http://localhost:3000/images1.jpg

--- d8/fileuploader/app.js
+++ d8/fileuploader/app.js
@@ -3,7 +3,7 @@
 const express = require('express');
 var app = express();
 
-app.use(express.static('uploads'));
+app.use(express.static('./d8/fileuploader/uploads'));
 
 const fileupload = require('express-fileupload');
 
